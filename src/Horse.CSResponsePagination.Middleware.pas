f854d6d826317a419461a754097018f9bc11ad03
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-05-19 23:50:22 -0300

    Inclusão do Objeto de Configurações

--- src/Horse.CSResponsePagination.Middleware.pas
+++ src/Horse.CSResponsePagination.Middleware.pas
@@ -13,10 +13,9 @@ uses
 
 procedure CSPaginationMiddleware(Req: THorseRequest; Res: THorseResponse; Next: TProc);
 
-var
-  global_paginateOnHeaders : Boolean;
-
 implementation
+
+uses Horse.CSResponsePagination;
 
 procedure CSPaginationMiddleware(Req: THorseRequest; Res: THorseResponse; Next: TProc);
 var
@@ -37,83 +36,88 @@ begin
 
   finally
 
-    if ( ( Req.Headers['limit'] <> '' ) and ( Req.Headers['offset'] <> '' ) ) then
+    if ( THorseHackResponse(Res).GetContent is TJSONArray ) then
     begin
 
-      try
-        _CSPPageLimit :=  StrToInt(Req.Headers['limit']);
-      except
-        _CSPPageLimit :=  50;
-      end;
-
-      try
-        _CSPOffset  :=  StrToInt(Req.Headers['offset']);
-      except
-        _CSPOffset :=  1;
-      end;
-
-      _CSPWebResponse := THorseHackResponse(Res).GetWebResponse;
-      _CSPContent := THorseHackResponse(Res).GetContent;
-
-      if Assigned(_CSPContent) and _CSPContent.InheritsFrom( TJSONValue ) then
+      if ( Req.Headers['limit'] <> '' ) then
       begin
 
         try
-
-          _CSPOriginalJsonArray := TJSONValue(_CSPContent) as TJSONArray;
-
-          TRY
-            IF (_CSPOriginalJsonArray.Count MOD _CSPPageLimit) > 0 THEN
-              _CSPTotalPages := (_CSPOriginalJsonArray.Count DIV _CSPPageLimit) + 1
-            ELSE
-              _CSPTotalPages := (_CSPOriginalJsonArray.Count DIV _CSPPageLimit);
-          EXCEPT
-            _CSPTotalPages := 1;
-          END;
-
-          _CSPNewJsonArray := TJsonArray.Create;
-
-          for _CSPInternalCount := (_CSPPageLimit * (_CSPOffset - 1)) to ((_CSPPageLimit * _CSPOffset)) - 1 do
-          begin
-            if _CSPInternalCount < _CSPOriginalJsonArray.Count then
-              _CSPNewJsonArray.AddElement(_CSPOriginalJsonArray.Items[_CSPInternalCount].Clone as TJSONValue);
-          end;
-
-          _CSPWebResponse.ContentType := 'application/json';
-
-          if global_paginateOnHeaders then
-          begin
-
-            Res.RawWebResponse.SetCustomHeader('X-Pagination-count',_CSPOriginalJsonArray.Count.ToString);
-            Res.RawWebResponse.SetCustomHeader('X-Pagination-pages',_CSPTotalPages.ToString);
-            Res.RawWebResponse.SetCustomHeader('X-Pagination-limit',_CSPPageLimit.ToString);
-            Res.RawWebResponse.SetCustomHeader('X-Pagination-offset',_CSPOffset.ToString);
-            Res.RawWebResponse.SetCustomHeader('X-Pagination-size',_CSPNewJsonArray.Count.ToString);
-
-            _CSPWebResponse.Content := _CSPNewJsonArray.ToJSON;
-
-            Res.Send( _CSPNewJsonArray );
-
-          end
-          else
-          begin
-
-            _CSPResponse := TJsonObject.Create;
-
-            _CSPResponse.AddPair('count', TJSONNumber.Create(_CSPOriginalJsonArray.Count));
-            _CSPResponse.AddPair('pages', TJSONNumber.Create(_CSPTotalPages));
-            _CSPResponse.AddPair('limit', TJSONNumber.Create(_CSPPageLimit));
-            _CSPResponse.AddPair('offset', TJSONNumber.Create(_CSPOffset));
-            _CSPResponse.AddPair('size', TJSONNumber.Create(_CSPNewJsonArray.Count));
-            _CSPResponse.AddPair('data', _CSPNewJsonArray);
-
-            _CSPWebResponse.Content := _CSPResponse.ToJSON;
-
-            Res.Send( _CSPResponse );
-
-          end;
-
+          _CSPPageLimit :=  StrToInt(Req.Headers['limit']);
         except
+          _CSPPageLimit :=  5;
+        end;
+
+        try
+          _CSPOffset  :=  StrToInt(Req.Headers['offset']);
+        except
+          _CSPOffset :=  1;
+        end;
+
+        _CSPWebResponse := THorseHackResponse(Res).GetWebResponse;
+        _CSPContent := THorseHackResponse(Res).GetContent;
+
+        if Assigned(_CSPContent) and _CSPContent.InheritsFrom( TJSONValue ) then
+        begin
+
+          try
+
+            _CSPOriginalJsonArray := TJSONValue(_CSPContent) as TJSONArray;
+
+            TRY
+              IF (_CSPOriginalJsonArray.Count MOD _CSPPageLimit) > 0 THEN
+                _CSPTotalPages := (_CSPOriginalJsonArray.Count DIV _CSPPageLimit) + 1
+              ELSE
+                _CSPTotalPages := (_CSPOriginalJsonArray.Count DIV _CSPPageLimit);
+            EXCEPT
+              _CSPTotalPages := 1;
+            END;
+
+            _CSPNewJsonArray := TJsonArray.Create;
+
+            for _CSPInternalCount := (_CSPPageLimit * (_CSPOffset - 1)) to ((_CSPPageLimit * _CSPOffset)) - 1 do
+            begin
+              if _CSPInternalCount < _CSPOriginalJsonArray.Count then
+                _CSPNewJsonArray.AddElement(_CSPOriginalJsonArray.Items[_CSPInternalCount].Clone as TJSONValue);
+            end;
+
+            _CSPWebResponse.ContentType := 'application/json';
+
+            if global_config.paginateOnHeaders then
+            begin
+
+              Res.RawWebResponse.SetCustomHeader(global_config.header.count,_CSPOriginalJsonArray.Count.ToString);
+              Res.RawWebResponse.SetCustomHeader(global_config.header.page,_CSPTotalPages.ToString);
+              Res.RawWebResponse.SetCustomHeader(global_config.header.limit,_CSPPageLimit.ToString);
+              Res.RawWebResponse.SetCustomHeader(global_config.header.offset,_CSPOffset.ToString);
+              Res.RawWebResponse.SetCustomHeader(global_config.header.size,_CSPNewJsonArray.Count.ToString);
+
+              _CSPWebResponse.Content := _CSPNewJsonArray.ToJSON;
+
+              Res.Send( _CSPNewJsonArray );
+
+            end
+            else
+            begin
+
+              _CSPResponse := TJsonObject.Create;
+
+              _CSPResponse.AddPair(global_config.body.count, TJSONNumber.Create(_CSPOriginalJsonArray.Count));
+              _CSPResponse.AddPair(global_config.body.page, TJSONNumber.Create(_CSPTotalPages));
+              _CSPResponse.AddPair(global_config.body.limit, TJSONNumber.Create(_CSPPageLimit));
+              _CSPResponse.AddPair(global_config.body.offset, TJSONNumber.Create(_CSPOffset));
+              _CSPResponse.AddPair(global_config.body.size, TJSONNumber.Create(_CSPNewJsonArray.Count));
+              _CSPResponse.AddPair(global_config.body.data, _CSPNewJsonArray);
+
+              _CSPWebResponse.Content := _CSPResponse.ToJSON;
+
+              Res.Send( _CSPResponse );
+
+            end;
+
+          except
+          end;
+
         end;
 
       end;
